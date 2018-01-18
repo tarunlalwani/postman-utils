b216f5d4b2b1a469aa74a001000e508b5ea184e7
Tarun Lalwani <tarunlalwani@users.noreply.github.com>
2018-01-18 11:53:13 +0530

    Update pmutils.js

--- pmutils.js
+++ pmutils.js
@@ -1,15 +1,29 @@
 if (typeof PMUtil === "undefined") {
     console.log("Initializing PMUtil for current session");
-    
+
     PMUtil = function () {
-        this.loadEnvironmentVariables = (url) => {
-            
-        }   
+        this.cache = {};
+
+        this.fetch = (url) => new Promise((resolve, reject) => pm.sendRequest(url, (err, res) => err ? reject([err, res]): resolve([err, res])))
+
+        this.get = this.fetch;
+
+        this.getBodyTemplate = (url) => url in this.cache ? cache[url] : this.get(url).then((err,res) => this.cache[url] = res.text() )
+
+        this.loadEnvironment = (url) => {
+            return new Promise( (resolve) => this.get(url).then(([err,res]) => {
+                var values = res.json().values;
+                for (var key in values)
+                    values[key].enabled ? pm.environment.set(values[key].key, values[key].value): null
+
+                resolve();
+            }));
+        };
         this.randomGenerator = () => {
             console.log("I generate random data");
         }
-    }
-    
+    };
+
     pmutil = new PMUtil();
 } else {
     console.log("PMUtils already loaded");
